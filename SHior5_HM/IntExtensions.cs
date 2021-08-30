55f36714d5a256b596c22db8037b75b4dbe8c59e
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-30 16:30:19 +0300

    Add files via upload

---
+++ SHior5_HM/IntExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHior5_HM
+{
+    public static class IntExtensions
+    {
+        public static string NumberToWords(this int number)
+        {
+            if (number > 100)
+                return "Ad Kan!";
+
+            if (number == 100)
+                return "One-Hundrend";
+
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+
+        public static long Hezka(this int number, int num)
+        {
+            long res = 1;
+            for (int i = 0; i < num; i++)
+            {
+                res *= number;
+            }
+            return res;
+        }
+        public static long MultipleBy(this int number, int num)
+        {
+            return number * num;
+        }
+
+    }
+}
---
+++ SHior5_HM/Medida.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHior5_HM
+{
+    class SortMedidaByTempDscAndCityAsc : IComparer<Medida>
+    {
+        public int Compare(Medida x, Medida y)
+        {
+            if (x.Temp.CompareTo(y.Temp) * -1 == 0)
+            {
+                return (x.City.CompareTo(y.City));
+            }
+            return (x.Temp.CompareTo(y.Temp) * -1);
+        }
+    }
+    class SortMedidaByLahotDsccAndTempAscAndCityAsc : IComparer<Medida>
+    {
+        public int Compare(Medida x, Medida y)
+        {
+            if (x.Lahot.CompareTo(y.Lahot) * -1 == 0)
+            {
+                if (x.Temp.CompareTo(y.Temp) * -1 == 0)
+                {
+                    return (x.City.CompareTo(y.City));
+                }
+                else
+                {
+                    return x.Temp.CompareTo(y.Temp) * -1;
+                }
+            }
+            return x.Lahot.CompareTo(y.Lahot) * -1;
+        }
+    }
+    class Medida : IComparable<Medida>
+    {
+        public string City { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public int Temp { get; set; }
+        public int Lahot { get; set; }
+
+        public Medida(string city, int temp)
+        {
+            City = city;
+            Temp = temp;
+        }
+
+        public int CompareTo(Medida other)
+        {
+            if (this.City.CompareTo(other.City) == 0)
+            {
+                return this.Date.CompareTo(other.Date);
+            }
+            return this.City.CompareTo(other.City);
+        }
+    }
+}
